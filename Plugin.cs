208cccdf3423f6883052e8ab8d91a6214bd4a41f
Kodi Craft <kodi@kdcf.me>
2023-12-04 21:57:54 +0100

    Initial Commit

---
+++ Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using GameNetcodeStuff;
+
+
+namespace TermMacros
+{
+    [BepInPlugin("me.kdcf.termspeak", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Lethal Company.exe")]
+    [HarmonyPatch(typeof(Terminal))]
+    public class TermSpeak : BaseUnityPlugin
+    {
+        private Harmony _harmony = new Harmony("me.kdcf.termspeak");
+        private static ManualLogSource _log = BepInEx.Logging.Logger.CreateLogSource("TermSpeak");
+        
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogError($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            _harmony.PatchAll(typeof(TermSpeak));
+        }
+
+        [HarmonyPatch(nameof(Terminal.BeginUsingTerminal))]
+        [HarmonyPrefix]
+        public static void OpenTerminal()
+        {
+            _log.LogInfo("Terminal opened!");
+        }   
+    }   
+}
---
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace TermMacros
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "TermSpeak";
+        public const string PLUGIN_NAME = "TermSpeak";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
