9c9eb19cfab77eaa55ce52ed1a6119a376188b5a
Kodi Craft <kodi@kdcf.me>
2023-12-05 00:57:39 +0100

    Remove generated files

--- obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace TermMacros
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_NAME = "TermSpeak";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
