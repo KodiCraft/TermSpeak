e51ac8264ef743815c77b2e1f758434b6861636d
Kodi Craft <kodi@kdcf.me>
2023-12-05 11:04:07 +0100

    Bump to 1.0.1

--- Plugin.cs
+++ Plugin.cs
@@ -5,7 +5,7 @@ using HarmonyLib;
 
 namespace TermMacros
 {
-    [BepInPlugin("me.kdcf.termspeak", "TermSpeak", "1.0.0")]
+    [BepInPlugin("me.kdcf.termspeak", "TermSpeak", "1.0.1")]
     [BepInProcess("Lethal Company.exe")]
     [HarmonyPatch(typeof(Terminal))]
     public class TermSpeak : BaseUnityPlugin
